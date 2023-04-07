3a6a927877828ee44286d950f0dbdc702767323a
SimplifiedUser <develop.polony@gmx.net>
2023-04-07 21:45:14 +0200

    Added API Endpoint

---
+++ src/routes/+server.ts
@@ -0,0 +1,37 @@
+import { error, json } from '@sveltejs/kit'
+import { Configuration, OpenAIApi } from "openai"
+
+export async function POST({ request }) {
+  
+  let data
+
+  try {
+    data = await request.json()
+  }
+  catch (exe) {
+    throw error(400)
+  }
+
+  if (data.message === undefined) throw error(400)
+  if (data.auth === undefined) throw error(401)
+  
+  const config = new Configuration({ apiKey: data.auth })
+  const openai = new OpenAIApi(config)
+
+  let answer
+
+  try {
+    answer = await openai.createChatCompletion({
+      model: data.model || "gpt-4",
+      messages: [{ role: "user", content: data.message }]
+    })
+  }
+  catch (exe: any) {
+    if (exe.response === undefined || exe.response.status === undefined) throw error(500)
+    throw error(exe.response.status)
+  }
+
+  if (answer.data.choices[0].message === undefined) throw error(500)
+
+  return json({ answer: answer.data.choices[0].message.content })
+}
